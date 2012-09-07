280844d7d8704049f7c2ee29161468032eb32d6d
dev-dexter <dev.dexter@gmail.com>
2012-09-07 13:31:32 +0200

    get it?

---
+++ src/org/devdexter/spr/rmi/plain/Hello.java
@@ -0,0 +1,15 @@
+package org.devdexter.spr.rmi.plain;
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+		System.out.println("Hello from Git Eclipse");
+
+	}
+
+}
